58b96280ed847d0f7b47b12f8b0b695782e86f21
zhangjin <919961049@qq.com>
2017-08-05 11:09:47 +0800

    完善登录界面，添加修改密码界面

---
+++ app/src/main/java/com/nankai/clubmanager/HomePageFragment.java
@@ -0,0 +1,20 @@
+package com.nankai.clubmanager;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhangjin on 2017/8/5.
+ */
+
+public class HomePageFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.homepage_fragment, container,false);
+    }
+}
