5ee92cbb267ed71c32c9c0e028b61a20577c4ce1
SwainTong <ybtxts@outlook.com>
2017-08-06 17:25:00 +0800

    添加了富文本编辑器

--- app/src/main/java/com/nankai/clubmanager/activity/ReleaseActivity.java
+++ app/src/main/java/com/nankai/clubmanager/activity/ReleaseActivity.java
@@ -1,10 +1,12 @@
-package com.nankai.clubmanager;
+package com.nankai.clubmanager.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.nankai.clubmanager.R;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
