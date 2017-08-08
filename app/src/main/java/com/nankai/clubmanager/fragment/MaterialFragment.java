a1065aeb124d67752a684a0bee14e865c3f45313
zhangjin <919961049@qq.com>
2017-08-08 18:48:25 +0800

    轮播图

--- app/src/main/java/com/nankai/clubmanager/fragment/MaterialFragment.java
+++ app/src/main/java/com/nankai/clubmanager/fragment/MaterialFragment.java
@@ -19,11 +19,11 @@ public class MaterialFragment extends Fragment {
 
     private ListView listView;
     private List<Map<String,Object>> lists=new ArrayList<>();
-    private int[] imgIds={R.drawable.head_image,R.drawable.head_image,R.drawable.head_image,
-            R.drawable.head_image,R.drawable.head_image,R.drawable.head_image,R.drawable.head_image,
-            R.drawable.head_image,R.drawable.head_image};
+    private int[] count={100,100,200,100,200,100,200,300,100};
     private String[] name={"张三","张三","张三",
             "张三","张三","张三","张三","张三","张三"};
+    private String[] extra={"张三","张三","张三",
+            "张三","张三","张三","张三","张三","张三"};  //备注
 
 
     @Override
@@ -31,16 +31,18 @@ public class MaterialFragment extends Fragment {
 
         View view=inflater.inflate(R.layout.material_fragment, container,false);
         listView = (ListView)view.findViewById(R.id.manage_material_listView);
-        String[] keys={"img","content"};
-        int[] ids={R.id.item_img_manage_material,R.id.item_content_manage_material};
+        String[] keys={"content","counts","extrax"};
+        int[] ids={R.id.manage_material_name,R.id.manage_material_count,R.id.manage_materail_extra};
 
         SimpleAdapter simpleAdapter=new SimpleAdapter(getActivity(),lists,R.layout.list_item_material,keys,ids);
         listView.setAdapter(simpleAdapter);
 
-        for(int i=0;i<imgIds.length;i++){
+        for(int i=0;i<count.length;i++){
             Map<String,Object> map=new HashMap<>();
-            map.put("img",imgIds[i]);
+
             map.put("content",name[i]);
+            map.put("counts",count[i]);
+            map.put("extrax",extra[i]);
             lists.add(map);
         }
         return view;
