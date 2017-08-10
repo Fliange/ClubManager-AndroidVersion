3d7d40f8c05c7de6d66dd6c2ca4b7977b08d7582
zhangjin <919961049@qq.com>
2017-08-10 14:56:26 +0800

    增加发现页面

--- app/src/main/java/com/nankai/clubmanager/fragment/FoundViewFragment.java
+++ app/src/main/java/com/nankai/clubmanager/fragment/FoundViewFragment.java
@@ -1,22 +1,52 @@
 package com.nankai.clubmanager.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.nankai.clubmanager.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhangjin on 2017/8/5.
  */
 
 public class FoundViewFragment extends Fragment{
+    private ListView listView;
+    private List<Map<String,Object>> lists=new ArrayList<>();
+    private String[] content={"这是显示在布告上的内容","这是显示在布告上的内容","这是显示在布告上的内容",
+            "这是显示在布告上的内容","这是显示在布告上的内容","这是显示在布告上的内容",
+            "这是显示在布告上的内容","这是显示在布告上的内容","这是显示在布告上的内容"};  //显示的内容
+
+    private String[] issuer={"张三","张三","张三",
+            "张三","张三","张三","张三","张三","张三"};  //发布者的名字
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.found_fragment, container,false);
+        View view=inflater.inflate(R.layout.found_fragment, container,false);
+        listView = (ListView)view.findViewById(R.id.found_listView);
+        String[] keys={"contents","issuers"};
+        int[] ids={R.id.issue_content,R.id.issuer};
+
+        SimpleAdapter simpleAdapter=new SimpleAdapter(getActivity(),lists,R.layout.issue_tips,keys,ids);
+        listView.setAdapter(simpleAdapter);
+
+        for(int i=0;i<content.length;i++){
+            Map<String,Object> map=new HashMap<>();
+
+            map.put("contents",content[i]);
+            map.put("issuers",issuer[i]);
+            lists.add(map);
+        }
+        return view;
     }
 }
